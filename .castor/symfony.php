8403993a2ab544b8f87b4403b9800750e2b50c9d
Tapomix <dev@tapomix.me>
2025-09-03 02:26:15 +0200

    [code] fix symfony castor task

--- .castor/symfony.php
+++ .castor/symfony.php
@@ -2,17 +2,16 @@
 
 namespace symfony;
 
-use Castor\Attribute\AsArgument;
+use Castor\Attribute\AsRawTokens;
 use Castor\Attribute\AsTask;
 
 use function Castor\context;
 
-/** @param string $cmd */
-#[AsTask(description: 'Excute console command', aliases: ['console'])]
+/** @param string[] $args */
+#[AsTask(description: 'Execute a Symfony Console command', aliases: ['console'])]
 function console(
-    #[AsArgument(description: 'Command to execute')]
-    ?string $cmd = null,
-): void
-{
-    \docker\run(SERVICE_PHP, \array_merge(['php', 'bin/console'], [(string) $cmd]), context()->withAllowFailure());
+    #[AsRawTokens]
+    array $args = [],
+): void {
+    \docker\exec(SERVICE_PHP, \array_merge(['php', 'bin/console'], $args), context('interactive'));
 }
