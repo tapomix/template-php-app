085c4b651fc6b51ff32c1b0fb98e10f7b1b4b211
Tapomix <dev@tapomix.me>
2025-09-16 17:11:13 +0200

    [code] add db:restore castor script

--- .castor/db.php
+++ .castor/db.php
@@ -5,7 +5,11 @@ namespace db;
 use Castor\Attribute\AsTask;
 
 use function Castor\context;
+use function Castor\fs;
 use function Castor\io;
+
+const DEFAULT_DB_USER = 'pg';
+const DEFAULT_DB_NAME = 'app';
 
 #[AsTask(description: 'Connect into PosgreSQL database', aliases: ['db', 'pg'])]
 function database(): void
@@ -20,7 +24,33 @@ function backup(string $timing): void
 {
     io()->title('Create DB backup');
 
-    $dumpFile = '/sql/dump-' . $_SERVER['CONTAINER_NAME'] . '-' . $timing . '.sql';
+    \docker\exec(SERVICE_DB, ['pg_dump', '-O', '-U', $_SERVER['DB_USER'] ?? 'pg', '-f', \db\buildDumpFileName($timing), $_SERVER['DB_NAME'] ?? 'app']);
+}
 
-    \docker\exec(SERVICE_DB, ['pg_dump', '-O', '-U', $_SERVER['DB_USER'] ?? 'pg', '-f', $dumpFile, $_SERVER['DB_NAME']]);
+function buildDumpFileName(string $timing, bool $local = false): string
+{
+    $dumpName = '/dump-' . $_SERVER['CONTAINER_NAME'] . '-' . $timing . '.sql';
+
+    return $local
+        ? __DIR__ . '/../.docker/db/sql' . $dumpName
+        : '/sql' . $dumpName
+    ;
+}
+
+#[AsTask(description: 'Restore database')]
+function restore(string $timing): void
+{
+    io()->title('Restore DB backup');
+
+    if (!fs()->exists(\db\buildDumpFileName($timing, true))) {
+        io()->error('Dump not found');
+
+        return;
+    }
+
+    \docker\exec(SERVICE_DB, ['dropdb', '-U', $_SERVER['DB_USER'] ?? DEFAULT_DB_USER, $_SERVER['DB_NAME'] ?? DEFAULT_DB_NAME]);
+    \docker\exec(SERVICE_DB, ['createdb', '-U', $_SERVER['DB_USER'] ?? DEFAULT_DB_USER, $_SERVER['DB_NAME'] ?? DEFAULT_DB_NAME]);
+    \docker\exec(SERVICE_DB, ['psql', '-U', $_SERVER['DB_USER'] ?? DEFAULT_DB_USER, '-f', \db\buildDumpFileName($timing), $_SERVER['DB_NAME'] ?? DEFAULT_DB_NAME]);
+
+    io()->success('Dump ' . $timing . ' loaded');
 }
